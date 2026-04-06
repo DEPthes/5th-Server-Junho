256de2afae59019e33e9973a1791137c053bd9d9
KimJunho <jhkim912@mju.ac.kr>
2026-04-06 17:20:23 +0900

    first commit

---
+++ Depth01/src/main/java/org/depth/LAB1.java
@@ -0,0 +1,13 @@
+package org.depth;
+
+public class LAB1 {
+
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+    public double add(double a, double b) { return a+b; }
+}
---
+++ Depth01/src/main/java/org/depth/Main.java
@@ -0,0 +1,13 @@
+package org.depth;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        LAB1 lab1 = new LAB1();
+        System.out.println(lab1.add(1,2));
+        System.out.println(lab1.add(1,2,3));
+        System.out.println(lab1.add(5.3, 3.8));
+    }
+}
+
